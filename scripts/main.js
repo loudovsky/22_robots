d9bcebfb3d7d442d630aca541788149bb9896bdf
loudovsky <luileptitecossais@gmail.com>
2024-02-15 16:05:20 +0100

    commit of the day

--- scripts/main.js
+++ scripts/main.js
@@ -8,14 +8,14 @@ fetch(`scripts/robots.json`)
     // Fonction pour afficher tous les utilisateurs
     function displayRobots() {
       data.robots.forEach(function(singleRobot){
-        gallery.innerHTML += `<div class="robot"><h4>${singleRobot.first_name} <strong>${singleRobot.last_name}</strong></h4><div class="image">
-        <img class="robot-img" src="${singleRobot.portrait}" title="${singleRobot.first_name}_${singleRobot.last_name}"></div><p>${singleRobot.description}</p></div>
+        gallery.innerHTML += `<div class="robot"><h4>${singleRobot.first_name} <strong>${singleRobot.last_name}</strong></h4><div class="image ${singleRobot.gender}"><img class="robot-img" src="${singleRobot.portrait}" title="${singleRobot.first_name}_${singleRobot.last_name}"><div class="robot__type"><strong>Type</strong> : ${singleRobot.type}</div><div class="robot__language"><strong>Langue parlée</strong> :<br>${singleRobot.language}</div></div><p>${singleRobot.description}</p></div>
         `
       })
     }
     displayRobots()
-    // Fonction pour afficher un utilisateur seul
+    /* Fonction pour afficher un utilisateur seul
     
+    */
       // Délégation d'événememnt sur gallery
       gallery.addEventListener('click', function(e){
         if(e.target.classList.contains('user-img')) {
